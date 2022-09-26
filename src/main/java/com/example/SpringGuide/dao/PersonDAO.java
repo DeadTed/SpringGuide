64d8a15e1206cb0ab0102a1d1bb1f7b354f3a8d0
yauheni.minchanka <IAMTED.BY1a>
2022-09-26 21:00:37 +0300

    added validation

--- src/main/java/com/example/SpringGuide/dao/PersonDAO.java
+++ src/main/java/com/example/SpringGuide/dao/PersonDAO.java
@@ -15,11 +15,13 @@ public class PersonDAO {
     private static int PEOPLE_COUNT;
     private List<Person> people;
 
-    public PersonDAO() {
+    {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Jery"));
-        people.add(new Person(++PEOPLE_COUNT, "Hardy"));
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
     }
 
     public List<Person> index() {
@@ -37,8 +39,12 @@ public class PersonDAO {
 
     public void update(int id, Person updatedPerson) {
         Person personToBeUpdated = show(id);
+
         personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
+
     public void delete(int id) {
         people.removeIf(p -> p.getId() == id);
     }
--- src/main/java/com/example/SpringGuide/models/Person.java
+++ src/main/java/com/example/SpringGuide/models/Person.java
@@ -2,7 +2,10 @@ package com.example.SpringGuide.models;
 
 import lombok.Data;
 
-import javax.annotation.security.DenyAll;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 /**
  * @author Yauheni Minchanka
@@ -11,13 +14,27 @@ import javax.annotation.security.DenyAll;
 @Data
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
 
+    @Min(value = 18, message = "Age should be greater than 18")
+    private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
     public Person() {
+
     }
 
-    public Person(int id, String name) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
+
 }
